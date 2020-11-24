f7eefa795420e3dba205ba5841a460adac749e54
drdr xp <drdr.xp@gmail.com>
2020-11-24 18:20:50 +0800

    release: v0.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="k3str",
     packages=["."],
-    version="0.1.2",
+    version="0.1.3",
     license='MIT',
     description='k3str is a collection of string utilities.',
     long_description='# k3str\n\n[![Build Status](https://travis-ci.com/pykit3/k3str.svg?branch=master)](https://travis-ci.com/pykit3/k3str)\n[![Documentation Status](https://readthedocs.org/projects/k3str/badge/?version=stable)](https://k3str.readthedocs.io/en/stable/?badge=stable)\n[![Package](https://img.shields.io/pypi/pyversions/k3str)](https://pypi.org/project/k3str)\n\nk3str is a collection of string utilities.\n\nk3str is a component of [pykit3] project: a python3 toolkit set.\n\n\n# Install\n\n```\npip install k3str\n```\n\n# Synopsis\n\n```python\n>>> repr(to_bytes(\'我\'))\n"b\'\\\\xe6\\\\x88\\\\x91\'"\n```\n\n#   Author\n\nZhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n#   Copyright and License\n\nThe MIT License (MIT)\n\nCopyright (c) 2015 Zhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n\n[pykit3]: https://github.com/pykit3',
@@ -14,7 +14,7 @@ setuptools.setup(
     keywords=['str', 'bytes'],
     python_requires='>=3.0',
 
-    install_requires=['semantic_version~=2.8.5', 'jinja2~=2.11.2', 'PyYAML~=5.3.1', 'sphinx~=3.3.1', 'k3ut==0.1.7'],
+    install_requires=['semantic_version~=2.8.5', 'jinja2~=2.11.2', 'PyYAML~=5.3.1', 'sphinx~=3.3.1', 'k3ut~=0.1.7'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
